cf54caf3298097285ec1ccd093d006e1dbdeb600
Shweyin Than <shweyin@gmail.com>
2018-03-13 18:43:44 -0400

    M1.1 Started OOP Assignment

---
+++ OOP Assignment/Date.h
@@ -0,0 +1,34 @@
+// Final Project Milestone 1
+//
+// Version 1.0
+// Date
+// Author
+// Description
+//
+//
+//
+//
+// Revision History
+// -----------------------------------------------------------
+// Name Shweyin          Date 3/16/2018            Reason 
+/////////////////////////////////////////////////////////////////
+#ifndef AMA_DATE_H
+#define AMA_DATE_H
+
+namespace AMA {
+
+	class Date {
+
+
+		int mdays(int, int)const;
+
+	public:
+
+	};
+
+
+
+
+
+}
+#endif
