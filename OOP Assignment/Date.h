bf9cf588260c2f3216041e2d69b2b916246c6b44
Shweyin Than <shweyin@gmail.com>
2018-03-13 21:10:16 -0400

    Added declarations to date class

--- OOP Assignment/Date.h
+++ OOP Assignment/Date.h
@@ -1,13 +1,5 @@
 // Final Project Milestone 1
 //
-// Version 1.0
-// Date
-// Author
-// Description
-//
-//
-//
-//
 // Revision History
 // -----------------------------------------------------------
 // Name Shweyin          Date 3/16/2018            Reason 
@@ -17,12 +9,28 @@
 
 namespace AMA {
 
+	const int min_year = 2000;
+	const int max_year = 2030;
+
 	class Date {
-
-
+		int year;
+		int month;
+		int day;
+		int comparator;
+		int errorState;
 		int mdays(int, int)const;
-
+		void errCode(int);
 	public:
+		Date();
+		Date(int, int, int);
+		bool operator==(const Date& rhs) const;
+		bool operator!=(const Date& rhs) const;
+		bool operator<(const Date& rhs) const;
+		bool operator>(const Date& rhs) const;
+		bool operator<=(const Date& rhs) const;
+		bool operator>=(const Date& rhs) const;
+		int errCode() const;
+		bool bad() const;
 
 	};
 
