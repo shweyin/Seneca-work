47b5d54d223b6e156c688f11af9622e3e09994e4
Shweyin Than <shweyin@gmail.com>
2018-03-15 17:12:14 -0400

    Finished Lists, and Images

---
+++ WEB222/Assignment 3/js/audio.js
@@ -0,0 +1,9 @@
+// Data for the "HTML Audio" Page
+
+var audio = {
+    controls: true, 
+    source: [
+        {src: "https://scs.senecac.on.ca/~patrick.crawford/shared/fall-2016/int222/Track03.mp3", type: "audio/mpeg"},
+        {src: "https://scs.senecac.on.ca/~patrick.crawford/shared/fall-2016/int222/Track03.ogg", type: "audio/ogg"}
+    ]
+};
---
+++ WEB222/Assignment 3/js/video.js
@@ -0,0 +1,12 @@
+// Data for the "HTML Video" Page
+
+var video = {
+    controls: true, 
+    width: 320,
+    height: 240,
+    source: [
+        {src: "https://scs.senecac.on.ca/~patrick.crawford/shared/fall-2016/int222/movie.mp4", type: "video/mp4"},
+        {src: "https://scs.senecac.on.ca/~patrick.crawford/shared/fall-2016/int222/movie.ogg", type: "video/ogg"},
+        {src: "https://scs.senecac.on.ca/~patrick.crawford/shared/fall-2016/int222/movie.webm", type: "video/webm"}
+    ]
+};
