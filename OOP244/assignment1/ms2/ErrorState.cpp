a58c7bf3b289fdb880fa2cd2f55f56d37cbbd015
Shweyin Than <shweyin@gmail.com>
2018-03-22 11:21:55 -0400

    Finished a1ms2

--- OOP244/assignment1/ms2/ErrorState.cpp
+++ OOP244/assignment1/ms2/ErrorState.cpp
@@ -2,28 +2,61 @@
 
 namespace AMA
 {
-	ErrorState::ErrorState(const char *)
+	ErrorState::ErrorState(const char* con_string)
 	{
+		if (con_string)
+		{
+			message(con_string);
+		}
+		else
+		{
+			clear();
+		}
 	}
 	ErrorState::~ErrorState()
 	{
+		error_msg_delete();
 	}
 	void ErrorState::clear()
 	{
+		error_msg_delete();
+		error_msg = new char[empty_size];
+		strcpy(error_msg, "Empty!");
+	}
+	void ErrorState::error_msg_delete()
+	{
+		if (error_msg)
+		{
+			delete[] error_msg;
+		}
 	}
 	bool ErrorState::isClear() const
 	{
-		return false;
+		return (strcmp(error_msg, "Empty!") == 0);
 	}
-	void ErrorState::message(const char *)
+	void ErrorState::message(const char* msg_string)
 	{
+		error_msg_delete();
+		int length = strlen(msg_string);
+		error_msg = new char[length + 1];
+		strcpy(error_msg, msg_string);
 	}
-	const char * ErrorState::message() const
+	const char* ErrorState::message() const
 	{
-		return nullptr;
+		return error_msg;
 	}
-	std::ostream & operator<<(const ErrorState &)
+	std::ostream& ErrorState::write(std::ostream& ostr) const
 	{
-		// TODO: insert return statement here
+		if (!ErrorState::isClear())
+		{
+			ostr << error_msg << std::endl;
+		}
+		return ostr;
+	}
+
+	std::ostream& operator<<(std::ostream& ostr, const ErrorState& ostr_ErrorState)
+	{
+		ostr_ErrorState.write(ostr);
+		return ostr;
 	}
 }
--- OOP244/assignment1/ms2/ErrorState.h
+++ OOP244/assignment1/ms2/ErrorState.h
@@ -1,4 +1,5 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
 #include <iomanip>
@@ -6,9 +7,12 @@
 
 namespace AMA
 {
+	const int empty_size = 7;
+
 	class ErrorState
 	{
 		char* error_msg;
+		void error_msg_delete();
 	public:
 		explicit ErrorState(const char* = nullptr);
 		ErrorState(const ErrorState&) = delete;
@@ -18,6 +22,7 @@ namespace AMA
 		bool isClear() const;
 		void message(const char*);
 		const char* message() const;
+		std::ostream& write(std::ostream&) const;
 	};
-	std::ostream& operator<<(const ErrorState&);
+	std::ostream& operator<<(std::ostream&, const ErrorState&);
 }
