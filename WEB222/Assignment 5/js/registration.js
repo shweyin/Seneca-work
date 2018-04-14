4621e9faa78b347d16637c5dc4ff69688a7792df
Shweyin Than <shweyin@gmail.com>
2018-04-14 19:12:45 -0400

    Finished working on web222 for now

--- WEB222/Assignment 5/js/registration.js
+++ WEB222/Assignment 5/js/registration.js
@@ -3,3 +3,46 @@ function checkform(myForm)
     var valid = false;
     return valid;
 }
+
+function username(myString)
+{
+    if(myString.length > 5)
+    {
+        clear();
+    }
+}
+
+function password(myString)
+{
+    
+}
+
+function phone(myString)
+{
+    
+}
+
+function email(myString)
+{
+    
+}
+function street(myString)
+{
+    
+}
+function city(myString)
+{
+    
+}
+function province(myString)
+{
+    
+}
+function postal(myString)
+{
+    
+}
+function errorMsg(errorString, HTMLobj)
+{
+
+}
